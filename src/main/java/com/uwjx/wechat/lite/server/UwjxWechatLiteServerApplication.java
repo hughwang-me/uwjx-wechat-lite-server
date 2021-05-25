5c5ee4d532046a7c90f4c4700fa8dd16f6ac8a5d
王欢 <18501667737@163.com>
2021-05-25 15:34:47 +0800

    init

---
+++ src/main/java/com/uwjx/wechat/lite/server/UwjxWechatLiteServerApplication.java
@@ -0,0 +1,13 @@
+package com.uwjx.wechat.lite.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UwjxWechatLiteServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UwjxWechatLiteServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uwjx/wechat/lite/server/UwjxWechatLiteServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uwjx.wechat.lite.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UwjxWechatLiteServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
