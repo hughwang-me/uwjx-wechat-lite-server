e4c549e5635b78a2c8c7762c51d0d78d7de07843
王欢 <18501667737@163.com>
2021-05-25 17:21:30 +0800

    init

--- src/main/java/com/uwjx/wechat/lite/server/controller/AccessTokenController.java
+++ src/main/java/com/uwjx/wechat/lite/server/controller/AccessTokenController.java
@@ -5,16 +5,18 @@ import com.uwjx.wechat.lite.server.util.GsonUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author wanghuan
  */
 @RestController
+@RequestMapping(value = "access_token")
 @Slf4j
 public class AccessTokenController {
 
-    @PostMapping
+    @PostMapping(value = "code")
     public String code(@RequestBody AccessToken accessToken){
         log.warn("请求参数:{}" , GsonUtil.toJsonString(accessToken));
 
